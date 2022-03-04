ce6754e4b532cf60adef14b25973ca09bdb89ff8
Carson Weeks <CarsonWeeks@mail.com>
2022-03-03 18:37:55 -0800

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('todos/', function () {
+    return view('todos');
+});
+
+Route::get('calendar/', function () {
+    return view('calendar');
+});
+
+Route::get('board/', function () {
+    return view('board');
+});
+
+Route::get('events-feed/', function () {
+    return '[
+        {
+          "title": "CSE4500 Class",
+          "start": "2022-02-23T17:30:00",
+          "end": "2022-02-23T18:45:00"
+        },
+        {
+          "title": "CSE4500 Class",
+          "start": "2022-02-28T17:30:00",
+          "end": "2022-02-28T18:45:00"
+        }
+      ]';
+});
+
+Route::fallback(function () {
+    return view('fallback');
+});
