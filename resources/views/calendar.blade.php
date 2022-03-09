071d01d02d0d9b82eeccd2093183b5f14c8a9e02
Carson Weeks <CarsonWeeks@mail.com>
2022-03-09 13:57:41 -0800

    changed query function.

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -8,7 +8,6 @@
 
 @section('content')
 
-          <p>{{ json_encode($events) }}</p>
 
 
 
@@ -36,7 +35,7 @@
 $( document ).ready(function() {
     var calendar = new FullCalendar.Calendar($('#calendar')[0], {
       initialView: 'dayGridMonth',
-      events: '/events-feed'
+      events: {events:  {{ json_encode($events) }}}
     });
     calendar.render();
 })
